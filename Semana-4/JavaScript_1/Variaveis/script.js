57bcd6e69f463a7a67d116ab39f7cc8843cf73e7
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2021-11-16 20:53:52 -0300

    f

---
+++ Semana-4/JavaScript_1/Variaveis/script.js
@@ -0,0 +1,12 @@
+var myName = "Jeovane";//Global
+let languagem = "JavaScript";//Local
+const patton = 10;//Constante não pode sofre atualizada 
+
+{
+    var age = 32
+}
+
+console.log(myName);
+
+
+
