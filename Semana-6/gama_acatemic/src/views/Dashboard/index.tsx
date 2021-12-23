28de23090e4180cf25415446d125890aab54f814
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2021-12-23 13:26:42 -0300

    Aluna da gama

--- Semana-6/gama_acatemic/src/views/Dashboard/index.tsx
+++ Semana-6/gama_acatemic/src/views/Dashboard/index.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React  from "react";
 import Footer from "../../components/Footer";
 import Nav from "../../components/Nav";
-import { Container } from "../Home/style";
+import { Container } from "./style";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import Loader from "../../components/Loade";
+
+interface ICollaborator{
+
+    id: number;
+    name: string;
+    email: string;
+    photo:{
+        path: string;
+    }
+}
+
+
+
 
 const Dashboard : React.FC = () =>{
+
+    const [data, setData] = useState<ICollaborator[]>([])
+    const [isLoadd, setIsLoad] = useState(false)
+    const tokem = localStorage.getItem("@gamaServiceToken")?.replace(/[*]/g,'')
+useEffect(() =>{
+    api.get("collaborator",{
+
+        headers:{
+            Authorization:`Bearer ${tokem}`,
+        }
+    }).then(response =>{
+        setIsLoad(true)
+        setData(response.data)
+    }).finally(() => setIsLoad(false))
+},[])
+    if(isLoadd){
+        return <Loader/>
+    }
+
     return(
         <div>
           
@@ -11,6 +46,19 @@ const Dashboard : React.FC = () =>{
             <Nav />
            <Container>
                 <h1>  Dashboard</h1>
+                <div className="card">
+
+                    {data?.map(el =>(
+                        <div key={el.id}>
+                            <img src={`https://sistemadeagendamento-san5v.ondigitalocean.app/tmp/uploads/${el.photo.path}`} alt="" />
+                            <p>
+                                {el.name}
+                            </p>
+                            <p>{el.email}</p>
+                        </div>
+                    ))}
+
+                </div>
 
                 </Container>
           
---
+++ Semana-6/gama_acatemic/src/views/Dashboard/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    font-family: "Roboto", sans-serif;
+
+    background-color: #8c52e5;
+
+    .card{
+        display: grid;
+    }
+
+`
