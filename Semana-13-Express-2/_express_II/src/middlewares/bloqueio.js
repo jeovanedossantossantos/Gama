f4cdaff59aad2eb17a96899ea286b8c4380cc1e8
Jeovane dos Santos Santos <jeovane.ssantos@outlook.com>
2022-04-10 10:21:39 -0300

    Novas aulas

--- Semana-13-Express-2/_express_II/src/middlewares/bloqueio.js
+++ Semana-13-Express-2/_express_II/src/middlewares/bloqueio.js
@@ -1,5 +1,7 @@
 module.exports = (req, res, next) =>{
     const {pass} = req.query;
+    const {token} = req.headers
+    console.log(token);
 
     if(!pass || pass !== "banana"){
         return res.status(400).json({"erro": "Não é banana"})
